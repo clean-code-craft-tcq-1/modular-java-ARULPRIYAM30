366a5387fa83ab54466e19035da40e644a1e8a43
ARULPRIYAM30 <77484841+ARULPRIYAM30@users.noreply.github.com>
2021-02-23 08:02:33 +0530

    Create Exchange.java

---
+++ colorcoder/Exchange.java
@@ -0,0 +1,25 @@
+package colorcoder;
+
+public class Exchange {
+	
+	static ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = 
+            (MajorColor) fromIndex(MajorColor.values(), zeroBasedPairNumber / ColorNames.numberOfMinorColors); 
+        MinorColor minorColor =
+            (MinorColor) fromIndex(MinorColor.values(), zeroBasedPairNumber % ColorNames.numberOfMinorColors);
+        return new ColorPair(majorColor, minorColor);
+    }
+    static int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major.getIndex() * ColorNames.numberOfMinorColors + minor.getIndex() + 1;
+    }
+    public static Color fromIndex(Object[] colors, int index) {
+        for(Object obj: colors) {
+        	Color color = (Color)obj;
+            if(color.getIndex() == index) {
+                return color;
+            }
+        }
+        return null;
+    }
+}
